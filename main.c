b3c7ae4f7f1f8fb0a8f046f66b8abb524868d383
leesuyeeeong <1913839@sookmyung.ac.kr>
2022-11-02 23:07:33 +0900

    실습5

--- main.c
+++ main.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 5
+#define ROWS 3
+#define COLS 3
+
 int main(void)
 {
-	int i;
-	int a[SIZE] = {1, 2, 3, 4, 5};
-	int b[SIZE] = {1, 2, 3, 4, 5};
-	int flag_same = 1;	
+	int A[ROWS][COLS] = { //더하기 대상 행렬
+ 		{2, 3, 0},
+ 		{8, 9, 1},
+ 		{7, 0, 5} };
+	int B[ROWS][COLS] = { //더하기 대상 행렬
+ 		{1, 0, 0},
+ 		{0, 1, 0},
+ 		{0, 0, 1} };
+	int C[ROWS][COLS]; //덧셈 결과 행렬 
 	
-	for (i=0; i<SIZE; i++){
-		if (a[i] != b[i])
-		{
-		printf("배열은 다른 값을 가집니다.");
-		flag_same = 0;
+	addMatrix(A, B, C);
+	printMatrix(C);
+	
+	return 0;
+}
+
+int i,j;
+void addMatrx(int A[][COLS], int B[][COLS], int 
+C[][COLS])
+{
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			C[i][j]=A[i][j] + B[i][j]
 		}
 	}
 }
+void printMatrx(int A[][COLS])
+{
+	printf("행렬의 합 : \n");
+    for(i=0;i<2;i++){
+     	for(j=0;j<2;j++){
+        	C[i][j]=A[i][j]+B[i][j];
+    	}
+}
+
+
 
